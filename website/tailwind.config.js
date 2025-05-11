51257feadbc7e8541ecaa0cade7c31a6c2fb2aa0
Cowsins <cowsinsgames@gmail.com>
2025-05-11 11:24:48 +0200

    Tailwind config Update

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,12 +2,14 @@ import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {},
   },
