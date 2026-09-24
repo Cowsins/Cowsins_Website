4711e5ccc4acae53fd1648c956cdff8b816f97b1
Cowsins <cowsinsgames@gmail.com>
2026-09-24 20:26:20 +0200

    Fixed Font on Dropdowns

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -1,8 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { fontSans, fontMono } from "@/config/fonts";
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark">
+    <Html lang="en" className={`dark ${fontSans.variable} ${fontMono.variable}`}>
       <Head />
       <body className="min-h-screen bg-background font-sans antialiased">
         <Main />
